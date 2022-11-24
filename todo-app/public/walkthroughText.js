1144a0d5bb8ffcc5d9fd803f8a1654de8a6102e0
Sean Blundell <seanblundell@gmail.com>
2022-11-24 12:30:01 +0100

    add in app guide

---
+++ todo-app/public/walkthroughText.js
@@ -0,0 +1,15 @@
+var walkthroughText = {
+  main: 'This app is currently using the real React. Checkout the branch chapter-1/step-1 to begin the journey to creating your own React.',
+  'chapter-1/step-1':
+    'Check out the render function in dom-handlers.js. This is where the rendering of elements to the DOM happens.',
+  'chapter-1/step-2': 'chapter-1/step-2',
+  'chapter-1/step-3': 'chapter-1/step-3',
+  'chapter-1/step-4': 'chapter-1/step-4',
+  'chapter-2/step-1': 'chapter-2/step-1',
+  'chapter-2/step-2': 'chapter-2/step-2',
+  'chapter-3/step-1': 'chapter-3/step-1',
+  'chapter-3/step-2': 'chapter-3/step-2',
+  'chapter-4/step-1': 'chapter-4/step-1',
+  'chapter-4/step-2': 'chapter-4/step-2',
+  final: '',
+};
