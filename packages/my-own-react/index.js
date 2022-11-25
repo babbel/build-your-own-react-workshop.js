645a87e825e42e16ba80cc0b1f77cfe2fb628281
jduthon <duthon.jean@gmail.com>
2022-11-25 17:59:41 +0100

    Fix small bug I introduced

--- packages/my-own-react/index.js
+++ packages/my-own-react/index.js
@@ -109,7 +109,7 @@ const renderComponentElement = (element, VDOM, VDOMPointer, hooks) => {
 };
 
 const renderPrimitive = (value, VDOM, VDOMPointer) => {
-  const VDOMElement = createVDOMElement(value);
+  const VDOMElement = createVDOMElement(value, VDOMPointer);
   setCurrentVDOMElement(
     VDOMPointer,
     VDOMElement,
